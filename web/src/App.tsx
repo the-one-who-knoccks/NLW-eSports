87171acc71ecd634150b42919fff7a65aa3b94e5
the-one-who-knoccks <thiagoalves.devp>
2022-09-12 17:21:07 -0300

    Initial web configuration

---
+++ web/src/App.tsx
@@ -0,0 +1,23 @@
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>{props.title}</button>
+  )
+}
+
+
+function App() {
+  return (
+   <>
+    <Button title="send 1"/>
+    <Button title="send 2"/>
+    <Button title="send 3"/>
+   </>
+  )
+}
+
+export default App;
